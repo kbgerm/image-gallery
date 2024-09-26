2fe9236af5e24d9285d4e91ada99e08a4c9eb207
kb_germ <k.b.germ1806@gmail.com>
2024-09-26 22:42:09 +0300

    form search complete

--- index.js
+++ index.js
@@ -19,16 +19,17 @@ async function getData() {
     {
       page: 1,
       client_id: 'sbETpyeTNDizQEv5RmN6w0LXhrXWAZf-R1g0nbmYyVw',
+      query: userRequest,
     }
   );
-  const res = await fetch(`https://api.unsplash.com/photos?${params.toString()}`);
+  const res = await fetch(`https://api.unsplash.com/search/photos?${params.toString()}`);
   images = await res.json();
   console.log(images);
 }
 
 function makeImg() {
 
-  for (let value of images) {
+  for (let value of images.results) {
     let img = document.createElement('img');
     img.src = value.urls.regular;
     img.alt = 'image';
